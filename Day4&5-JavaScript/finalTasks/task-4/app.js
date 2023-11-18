2d9506c33f9ebb31b6ac6cd81f90c6b4972b2848
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-11-19 02:24:19 +0800

    added screenshot result

--- Day4&5-JavaScript/finalTasks/task-4/app.js
+++ Day4&5-JavaScript/finalTasks/task-4/app.js
@@ -11,4 +11,5 @@ const msg = ['shahrul', 'arif', 'aaron'];
 btn.addEventListener('click', () => {
   /* display random array item */
   result.textContent = 'Hi from ' + msg[Math.floor(Math.random() * msg.length)];
+  console.log('Hi from ' + msg[Math.floor(Math.random() * msg.length)]);
 });
