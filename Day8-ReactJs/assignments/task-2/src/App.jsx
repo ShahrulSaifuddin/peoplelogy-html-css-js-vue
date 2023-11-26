9c60723e5ef3a1b2fb0a26dc7fef76fc357435ce
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-11-26 16:46:43 +0800

    clean up code

--- Day8-ReactJs/assignments/task-2/src/App.jsx
+++ Day8-ReactJs/assignments/task-2/src/App.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaQuoteRight } from 'react-icons/fa';
 
 function App() {
