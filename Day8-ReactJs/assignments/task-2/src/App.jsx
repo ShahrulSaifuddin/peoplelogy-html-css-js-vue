7646a01aaf3e28a11ef768aa38e73f5add585df2
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-11-27 00:30:56 +0800

    leave commend for the code

--- Day8-ReactJs/assignments/task-2/src/App.jsx
+++ Day8-ReactJs/assignments/task-2/src/App.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react';
 import { FaQuoteRight } from 'react-icons/fa';
 
 function App() {
+  // State variables to manage data and loading state
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  // Function to fetch random user data
   const fetchUserData = async () => {
     try {
       setIsLoading(true);
@@ -17,10 +19,12 @@ function App() {
     }
   };
 
+  // useEffect hook to fetch user data when the component mounts
   useEffect(() => {
     fetchUserData();
   }, []);
 
+  // Loading state rendering
   if (isLoading) {
     return (
       <div className="loader-container">
@@ -29,11 +33,12 @@ function App() {
     );
   }
 
+  // Main component rendering
   return (
     <main>
       <article className="review">
         <div className="img-container">
-          <img src={data?.picture?.large} className="person-img" />
+          <img src={data?.picture?.large} className="person-img" alt="user" />
           <span className="quote-icon">
             <FaQuoteRight />
           </span>
@@ -43,6 +48,7 @@ function App() {
         </h4>
         <p className="email">{data?.email}</p>
         <p className="cell">+{data?.cell}</p>
+        {/* Button to fetch new random user data */}
         <button className="btn btn-hipster" onClick={() => fetchUserData()}>
           random user
         </button>
