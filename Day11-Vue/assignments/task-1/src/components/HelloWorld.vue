167e6643acbf61cc5b197f2f56b10e2077b1e45f
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-12-09 13:00:38 +0800

    finish vue task 1

---
+++ Day11-Vue/assignments/task-1/src/components/HelloWorld.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <section>
+      <h1>Form</h1>
+      <form @submit.prevent>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          v-model="enteredName"
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Enter your email"
+          v-model="enteredEmail"
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Enter your password"
+          v-model="enteredPassword"
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Password</th>
+        </tr>
+        <tr>
+          <td>{{ enteredName }}</td>
+          <td>{{ enteredEmail }}</td>
+          <td>{{ enteredPassword }}</td>
+        </tr>
+      </table>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      enteredName: '',
+      enteredEmail: '',
+      enteredPassword: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+  display: grid;
+  place-items: center;
+}
+form {
+  margin: 3rem 0;
+}
+input {
+  margin: 0.25rem 0;
+  width: 100%;
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+}
+button {
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  background: transparent;
+  transition: all 0.3s ease-out;
+  margin-top: 1rem;
+}
+button:hover {
+  background: black;
+  color: white;
+}
+table,
+tr,
+th,
+td {
+  border: 1px solid black;
+  padding: 1rem;
+}
+
+table {
+  border-collapse: collapse;
+}
+</style>
--- Day11-Vue/hello-world/src/components/MakeHome.vue
+++ Day11-Vue/hello-world/src/components/MakeHome.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>{{ displayName() }}</h1>
+  <h1 :style="{ color: isColorDefault ? 'black' : 'red' }">
+    {{ displayName() }}
+  </h1>
+  <button @click="toggleColor">Click me</button>
+  <hr />
+  <form @submit.prevent>
+    <input type="text" placeholder="Enter your name" v-model="enteredName" />
+    <br />
+    <input type="email" placeholder="Enter your email" v-model="enteredEmail" />
+    <br />
+    <button type="submit" @click="submit">submit</button>
+  </form>
 </template>
 
 <script>
@@ -7,12 +18,25 @@ export default {
   data() {
     return {
       name: 'World',
+      isColorDefault: true,
+      enteredName: '',
+      enteredEmail: '',
     };
   },
   methods: {
     displayName() {
       return `Hello ${this.name}`;
     },
+    toggleColor() {
+      this.isColorDefault = !this.isColorDefault;
+    },
+    submit() {
+      console.log(this.enteredName, this.enteredEmail);
+      this.enteredEmail = '';
+      this.enteredName = '';
+    },
   },
 };
 </script>
+
+<style></style>
