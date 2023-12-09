9f46b2bb7ea643c36a7008cf7ab47c8cf2481854
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-12-09 11:27:41 +0800

    add hello world project

---
+++ Day11-Vue/hello-world/src/components/MakeHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ displayName() }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'World',
+    };
+  },
+  methods: {
+    displayName() {
+      return `Hello ${this.name}`;
+    },
+  },
+};
+</script>
