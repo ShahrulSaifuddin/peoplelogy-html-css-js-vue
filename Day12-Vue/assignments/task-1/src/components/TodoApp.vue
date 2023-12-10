e3f9fc32ac0d7d1fa61e0fde6f5307a798b73a2e
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-12-10 10:22:41 +0800

    task 1

---
+++ Day12-Vue/assignments/task-1/src/components/TodoApp.vue
@@ -0,0 +1,85 @@
+<template>
+  <h1>Todo App</h1>
+  <form @submit.prevent>
+    <input
+      type="text"
+      placeholder="Enter your todo list here"
+      v-model="newTodo"
+    />
+    <button type="submit" @click="addTodo">Add Task</button>
+  </form>
+  <div class="lists-container">
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.name }}
+        <span>
+          <button @click="removeTodo(todo.id)">Remove</button>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { nanoid } from 'nanoid';
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      const newItem = { id: nanoid(), name: this.newTodo.trim() };
+      this.todos.push(newItem);
+    },
+    removeTodo(itemId) {
+      const index = this.todos.findIndex((todo) => todo.id === itemId);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-transform: uppercase;
+}
+.lists-container {
+  display: flex;
+  justify-content: center;
+}
+ul {
+  width: min(600px, 70%);
+}
+li {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+  text-transform: capitalize;
+}
+form {
+  margin: 2rem 0;
+}
+input {
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  margin-right: 1rem;
+}
+button {
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  background: transparent;
+  cursor: pointer;
+  transition: all 0.3s ease-out;
+}
+
+button:hover {
+  background: black;
+  color: white;
+}
+</style>
