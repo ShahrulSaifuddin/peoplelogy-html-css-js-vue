feb484c84eb51c60b23d7b7a9b7294ea8e23acc0
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2023-12-10 12:52:17 +0800

    checkout form

--- Day12-Vue/tutorial/src/App.vue
+++ Day12-Vue/tutorial/src/App.vue
@@ -2,18 +2,22 @@
   <div class="container">
     <!-- <TodoApp /> -->
     <!-- <hr /> -->
-    <CounterDownApp />
+    <!-- <CounterDownApp /> -->
+    <!-- <hr> -->
+    <CheckoutFormApp />
   </div>
 </template>
 
 <script>
 // import TodoApp from './components/TodoApp.vue';
-import CounterDownApp from './components/CounterDownApp.vue';
+// import CounterDownApp from './components/CounterDownApp.vue';
+import CheckoutFormApp from './components/CheckoutFormApp.vue';
 export default {
   name: 'App',
   components: {
     // TodoApp,
-    CounterDownApp,
+    // CounterDownApp,
+    CheckoutFormApp,
   },
 };
 </script>
@@ -39,5 +43,58 @@ export default {
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   border-radius: 0.25rem;
   padding: 2rem 1rem;
+}
+
+/* CSS */
+.button-16 {
+  touch-action: manipulation;
+  all: unset;
+  position: relative;
+  border-radius: 50px;
+  padding: 0.5rem 1rem;
+  background-color: #f0f7fa;
+  box-shadow: inset 0 -4px 8px #87bfd8, 0 4px 6px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  color: #0470e3;
+  transition: transform 0.4s cubic-bezier(0.55, 1, 0.15, 1);
+  will-change: transform;
+  margin: 2rem 0;
+}
+.button-16:active {
+  transform: scale(0.92);
+}
+.button-16:active::after {
+  opacity: 1;
+}
+.button-16::after {
+  content: '';
+  position: absolute;
+  inset: 0;
+  border-image-source: url('https://csspro.com/css-3d-buttons/buttons-img/mario-button-borders.webp');
+  border-image-slice: 50%;
+  border-image-width: 40px;
+  border-style: solid;
+  transform: scale(1.04, 1.08);
+  opacity: 0;
+  transition-property: opacity;
+  transition-duration: 0.4s;
+  transition-timing-function: cubic-bezier(0.55, 1, 0.15, 1);
+  will-change: transform;
+}
+.button-16:focus-visible::after {
+  opacity: 1;
+  animation: pulsate-16 1s infinite;
+}
+
+@keyframes pulsate-16 {
+  0% {
+    transform: scale(1.04, 1.08);
+  }
+  50% {
+    transform: scale(1.08, 1.12);
+  }
+  100% {
+    transform: scale(1.04, 1.08);
+  }
 }
 </style>
---
+++ Day12-Vue/tutorial/src/components/CheckoutFormApp.vue
@@ -0,0 +1,88 @@
+<template>
+  <h1>checkout form</h1>
+  <form @submit.prevent>
+    <div class="form-group">
+      <label for="name">name:</label>
+      <input v-model="formData.name" type="text" id="name" required />
+    </div>
+    <div class="form-group">
+      <label for="email">email:</label>
+      <input v-model="formData.email" type="email" id="email" required />
+    </div>
+    <div class="form-group">
+      <label for="shipping">shipping address:</label>
+      <textarea
+        v-model="formData.shipping"
+        type="text"
+        id="shipping"
+        rows="5"
+        required
+      ></textarea>
+    </div>
+    <div class="form-group">
+      <label for="credit">credit card:</label>
+      <input v-model="formData.credit" type="text" id="credit" required />
+    </div>
+    <button @click="submitForm" class="button-16" role="button">
+      <span class="text">Submit</span>
+    </button>
+  </form>
+  <div v-show="orderSubmitted">
+    <h2>order submited!</h2>
+    <p><b>name: </b>{{ formData.name }}</p>
+    <p><b>email: </b>{{ formData.email }}</p>
+    <p><b>shipping address: </b>{{ formData.shipping }}</p>
+    <p><b>credit cars: </b>{{ formData.credit }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        shipping: '',
+        credit: '',
+      },
+      orderSubmitted: false,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.orderSubmitted = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-transform: uppercase;
+}
+.form-group {
+  display: grid;
+  text-align: start;
+  margin-top: 1.5rem;
+  gap: 0.5rem;
+}
+label {
+  text-transform: capitalize;
+  font-weight: bolder;
+}
+input,
+textarea {
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  resize: vertical;
+}
+h2 {
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+}
+p {
+  text-transform: capitalize;
+  margin-top: 0.25rem;
+}
+</style>
